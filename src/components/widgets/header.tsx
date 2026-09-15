8af2a54abcaaab7c7ee4c6a6cf4685c971f4f697
RyannKim327 <rksesgundo123@gmail.com>
2026-09-16 07:03:31 +0800

    09-16-26  7:03 [initially added data]

--- src/components/widgets/header.tsx
+++ src/components/widgets/header.tsx
@@ -1,21 +1,26 @@
+import { House, Info, Search, User } from "lucide-react"
 import { Link } from "react-router"
 
 const navs = [
   {
     title: "Home",
-    endpoint: ""
+    endpoint: "/",
+    icon: <House />
   },
   {
     title: "About",
-    endpoint: "about"
+    endpoint: "/about",
+    icon: <Info />
   },
   {
     title: "Search",
-    action: "search"
+    action: "search",
+    icon: <Search />
   },
   {
     title: "Login",
-    action: "login"
+    action: "login",
+    icon: <User />
   }
 ]
 
@@ -27,22 +32,29 @@ export default function Header({ setAction }: header) {
   return (
     <div className="flex justify-between items-center w-full h-10 bg-secondary-bg border-b-2 border-b-solid border-border px-2 sticky top-0 z-10 mb-2">
       <span>Clio</span>
+
       <nav className="flex gap-2">
         {navs.map((nav, i: number) => {
           return (
             nav.endpoint ?
               <Link
                 key={`${i}. ${nav.title}`}
-                to={nav.endpoint}>{nav.title}</Link> :
+                to={nav.endpoint}>
+                <span className="hidden md:inline">{nav.title}</span>
+                <span className="md:hidden">{nav.icon}</span>
+              </Link> :
               <li
                 onClick={() => {
                   setAction(nav.action ?? "")
                 }}
                 key={`${i}. ${nav.title}`}
-                className="list-none">{nav.title}</li>
+                className="list-none">
+                <span className="hidden md:inline">{nav.title}</span>
+                <span className="md:hidden">{nav.icon}</span>
+              </li>
           )
         })}
       </nav>
-    </div>
+    </div >
   )
 }
--- src/routes/reader/index.tsx
+++ src/routes/reader/index.tsx
@@ -1,17 +1,20 @@
 import Book from "@/components/ui/book";
 import Header from "@/components/widgets/header";
+import { stories } from "@/data/stories";
 import { useEffect, useState } from "react";
 
 export default function Reader() {
   const [action, setAction] = useState("")
 
-  useEffect(() => { }, [action])
-
   return (
     <div className="flex flex-col bg-bg text-fg w-dvw h-dvh overflow-hidden">
       <Header setAction={setAction} />
       <div className="grid grid-cols-5 gap-2 m-2 w-[calc(90%-1rem)]">
-        <Book title="sanaol santol" author="mang kaknorr" cover="https://wallpapers.com/images/high/iu-in-brown-coat-2b59nkxx8y8hpdes.webp" />
+        {
+          stories.map((story, i: number) => {
+            return <Book key={`${i}. ${story.title}`} title={story.title} author={story.author} cover={story.cover} />
+          })
+        }
       </div>
     </div>
   )
