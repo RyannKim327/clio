89460c426227d825b0a6982908ddf72a473b0184
RyannKim327 <rksesgundo123@gmail.com>
2026-09-19 22:04:18 +0800

    09-19-26 22:04 [feat: chapter?]

--- src/routes/reader/read.tsx
+++ src/routes/reader/read.tsx
@@ -2,15 +2,17 @@ import { stories } from "@/data/stories";
 import { markdownToHtml } from "@/lib/markdown";
 import { splitSegmentsByStartEnd } from "@/lib/split-segments";
 import { ArrowLeft } from "lucide-react";
-import { Link, useParams, useSearchParams } from "react-router";
+import { Link, useLocation, useNavigate, useParams, useSearchParams } from "react-router";
 
 export default function Read() {
   const [searchParams] = useSearchParams()
   const { id } = useParams()
+  const location = useLocation()
+  const navigate = useNavigate()
   const chapter = searchParams.get("c") ?? "chapter 1"
   const story = stories[parseInt(id)]
 
-  const chapters = splitSegmentsByStartEnd(story.chapters[chapter])
+  const chapters = splitSegmentsByStartEnd(story.chapters[chapter.toLowerCase()])
 
   return (
     <div className="flex flex-col items-center w-full h-full gap-2 overflow-x-hidden">
@@ -25,13 +27,29 @@ export default function Read() {
         </div>
       </div>
 
-      <div className="grid grid-cols-3 w-[calc(90%-1rem)] h-full overflow-hidden">
-        <div className="col-span-1 flex flex-col overflow-y-auto sticky">
-          <span>Test</span>
+      <div className="flex w-[calc(90%-1rem)] justify-between h-full overflow-hidden">
+        <div className="flex flex-col w-[calc(25%-1rem)] overflow-y-auto sticky">
+          <div className="sticky top-0 bg-secondary-bg p-2 text-center mb-5">
+            Chapter Lists
+          </div>
+          {
+            Object.keys(story.chapters).map(chap => {
+              return (
+                <span
+                  className={`border-b border-b-solid border-fg hover:border-border hover:text-border p-2 cursor-pointer ${chap.toLowerCase() === chapter.toLowerCase() ? "border-b-border text-border" : ""}`}
+                  onClick={() => {
+                    const params = new URLSearchParams(location.search)
+                    params.set("c", chap)
+                    navigate(`${location.pathname}?${params.toString()}`)
+                  }}
+                > {chap.toUpperCase()}</span>
+              )
+            })
+          }
         </div>
-        <div className="col-span-2 flex flex-col gap-24 overflow-x-hidden scrollbar-thin scrollbar-thumb-secondary-bg snap-mandatory snap-y">
+        <div className="flex flex-col w-[calc(70%-1rem)] gap-24 overflow-x-hidden scrollbar-thin scrollbar-thumb-secondary-bg snap-mandatory snap-y">
           <div className="sticky top-0 bg-secondary-bg p-2 text-center">
-            Chapter {chapter}
+            {chapter.toUpperCase()}
           </div>
           {
             chapters.map((line) => {
